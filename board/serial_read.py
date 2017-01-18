528dac1238ddd0dd32ac05f1e0024ea5e8101a01
Vasu Agrawal <vasu_agrawal@yahoo.com>
2017-01-17 19:17:23 -0500

    Server understands board serial read now.

--- board/serial_read.py
+++ board/serial_read.py
@@ -5,25 +5,32 @@
 import serial
 import json
 import socket
+import logging
 
+# Since the inputs should be coming in fairly slowly, a new connection is going
+# to be opened for every single keypress.
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("localhost", 8000))
-
-    out = dict()
-    out['type'] = 'KEY_PRESS'
-
     with serial.Serial('/dev/ttyACM0', 115200) as ser:
         while True:
             # We should read a row, then a column.
             row = ser.read() 
             col = ser.read()
 
-            out['payload'] = "%d:%d" % (int(row), int(col))
-            payload = json.dumps(out)
-           
-            s.send(payload.encode('ascii'))
-            print(payload)
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect(("localhost", 8000))
+
+                out = dict()
+                out['type'] = 'KEY_PRESS'
+                out['payload'] = "%d:%d" % (int(row), int(col))
+                payload = json.dumps(out)
+            
+                s.send(payload.encode('ascii'))
+                print(payload)
+
+                s.close()
+            except ConnectionRefusedError:
+                logging.error("Unable to connect to server!")
 
 
 if __name__ == "__main__":
